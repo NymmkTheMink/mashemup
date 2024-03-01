92b9f386f95f82294862be1caad3e6e9f783e500
NymmkTheMink <141179443+NymmkTheMink@users.noreply.github.com>
2024-03-01 10:12:15 +0100

    first commit

---
+++ src/main/java/com/mashemup/mashemup/MashemupApplication.java
@@ -0,0 +1,13 @@
+package com.mashemup.mashemup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MashemupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MashemupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mashemup/mashemup/MashemupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mashemup.mashemup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MashemupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
