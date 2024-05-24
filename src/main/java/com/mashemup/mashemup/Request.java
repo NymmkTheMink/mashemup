a18454a5974801677868886293fde6a9a9075baf
NymmkTheMink <141179443+NymmkTheMink@users.noreply.github.com>
2024-05-24 22:01:00 +0200

    Create Request.java

---
+++ src/main/java/com/mashemup/mashemup/Request.java
@@ -0,0 +1,14 @@
+package com.mashemup.mashemup;
+
+import java.util.List;
+
+public class Request {
+    private String name;
+    private String email;
+    private int amountSongs;
+    private List<SongCharacterPair> songCharacterPairs;
+    private String additionalInfo;
+
+    // getters and setters for all fields
+}
+
