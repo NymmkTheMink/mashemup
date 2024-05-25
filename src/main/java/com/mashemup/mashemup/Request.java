4e536eab0395b10a760772be9cd41a3ae027d6dd
NymmkTheMink <141179443+NymmkTheMink@users.noreply.github.com>
2024-05-25 23:13:22 +0200

    Update Request.java

--- src/main/java/com/mashemup/mashemup/Request.java
+++ src/main/java/com/mashemup/mashemup/Request.java
@@ -6,9 +6,23 @@ public class Request {
     private String name;
     private String email;
     private int amountSongs;
-    private List<SongCharacterPair> songCharacterPairs;
+    private List<SongInput> songInputs;
     private String additionalInfo;
 
     // getters and setters for all fields
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public int getAmountSongs() { return amountSongs; }
+    public void setAmountSongs(int amountSongs) { this.amountSongs = amountSongs; }
+
+    public List<SongInput> getSongInputs() { return songInputs; }
+    public void setSongInputs(List<SongInput> songCharacterPairs) { this.songInputs = songCharacterPairs; }
+
+    public String getAdditionalInfo() { return additionalInfo; }
+    public void setAdditionalInfo(String additionalInfo) { this.additionalInfo = additionalInfo; }
 }
 
