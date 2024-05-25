de66faac89821b35718fceac991c49c8e2f867d9
NymmkTheMink <141179443+NymmkTheMink@users.noreply.github.com>
2024-05-25 23:13:25 +0200

    Update RequestController.java

---
+++ src/main/java/com/mashemup/mashemup/RequestController.java
@@ -0,0 +1,37 @@
+package com.mashemup.mashemup;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/requests")
+public class RequestController {
+
+    private final RequestRepository requestRepository;
+
+    @Autowired
+    public RequestController(RequestRepository requestRepository) {
+        this.requestRepository = requestRepository;
+    }
+
+    @PostMapping("/submit")
+    public ResponseEntity<String> submitRequest(@RequestBody Request request) {
+        requestRepository.saveRequest(request);
+        return new ResponseEntity<>("Request submitted successfully", HttpStatus.CREATED);
+    }
+
+    @GetMapping("/all")
+    public List<Request> getAllRequests() {
+        return requestRepository.getAllRequests();
+    }
+
+    @DeleteMapping("/deleteAll")
+    public ResponseEntity<String> deleteAllRequests() {
+        requestRepository.deleteAllRequests();
+        return new ResponseEntity<>("All requests deleted successfully", HttpStatus.OK);
+    }
+}
